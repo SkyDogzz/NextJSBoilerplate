d6934c0938eb94f8aa899bdb62a511e3d5e9bd97
SkyDogzz <thomas.stephan@live.fr>
2024-05-03 04:04:01 +0200

    Refactor file paths in tsconfig.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={roboto.className}>{children}</body>
+      </html>
   );
 }
